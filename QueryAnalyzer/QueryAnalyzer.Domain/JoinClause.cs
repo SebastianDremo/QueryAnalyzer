94d103c32ff797a63933815c12cf16f9e8afcf1c
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2022-05-18 14:52:04 +0200

    Created new model of a query with join

---
+++ QueryAnalyzer/QueryAnalyzer.Domain/JoinClause.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryAnalyzer.Domain
+{
+    class JoinClause
+    {
+        public JoinPart LeftPart { get; set; }
+
+        public JoinPart RightPart { get; set; }
+    }
+}
---
+++ QueryAnalyzer/QueryAnalyzer.Domain/Query.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryAnalyzer.Domain
+{
+    class Query
+    {
+        string FromTable { get; set; }
+
+        List<Join> Joins { get; set; }
+    }
+}
