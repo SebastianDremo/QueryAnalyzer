8f0a698c3f3838ff29aa2ecd076bf52380740792
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2022-05-18 16:44:48 +0200

    Created relationship model and methods to build it

---
+++ QueryAnalyzer/QueryAnalyzer.Domain/Relationship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryAnalyzer.Domain
+{
+    class Relationship
+    {
+        public string PKTable { get; set; }
+
+        public string FKTable { get; set; }
+
+        public List<string> PKColumns { get; set; }
+
+        public List<string> FKColumns { get; set; }
+    }
+}
---
+++ QueryAnalyzer/QueryAnalyzer.Domain/RelationshipBuilder.cs
@@ -0,0 +1,52 @@
+﻿namespace QueryAnalyzer.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RelationshipBuilder
+    {
+        
+        public List<Relationship> BuildRelationshipsFromQuery(Query query)
+        {
+            List<Relationship> relationships = new List<Relationship>();
+
+            foreach(var join in query.Joins)
+            {
+                relationships.Add(BuildRelationshipFromJoin(query.FromTable, join));
+            }
+        }
+
+        private Relationship BuildRelationshipFromJoin(string fromTable, Join join)
+        {
+            string pkTable = fromTable;
+            string fkTable = join.JoinedTable;
+            List<string> pkColumns = new List<string>();
+            List<string> fkColumns = new List<string>();
+
+            foreach(JoinClause joinClause in join.JoinClauses)
+            {
+                if(joinClause.LeftPart.Table.Equals(pkTable))
+                {
+                    pkColumns.Add(joinClause.LeftPart.Column);
+                    fkColumns.Add(joinClause.RightPart.Column);
+                }
+                else
+                {
+                    pkColumns.Add(joinClause.RightPart.Column);
+                    fkColumns.Add(joinClause.LeftPart.Column);
+                }
+            }
+
+            return new Relationship()
+            {
+                PKTable = pkTable,
+                FKTable = fkTable,
+                PKColumns = pkColumns,
+                FKColumns = fkColumns
+            };
+        }
+    }
+}
