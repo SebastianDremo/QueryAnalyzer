cc80502e8bc0fb7af65d0e4568e8676555a231a1
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2022-05-18 23:25:24 +0200

    Fixed proposition service

--- QueryAnalyzer/QueryAnalyzer.Service/PropositionsService.cs
+++ QueryAnalyzer/QueryAnalyzer.Service/PropositionsService.cs
@@ -1,19 +1,29 @@
-﻿namespace QueryAnalyzer.Service;
+﻿using QueryAnalyzer.Domain;
+
+namespace QueryAnalyzer.Service;
 
 public class PropositionsService
 {
+    public string ProposeForeignKey(Relationship relationship)
+    {
+        return ProposeForeignKey(
+                "pkSchema", //replace with relationship.PKSchema after merge
+                relationship.PKTable, 
+                relationship.PKColumns,
+                "fkSchema", //relationship.FKSChema
+                relationship.FKTable, 
+                relationship.FKColumns);
+               
+    }
     //its messy right here but I just want to go to sleep at this point
-    public void ProposeForeignKey(string pkSchema, string pkTable, List<string> pkColumns, string fkSchema, string fkTable, List<string> fkColumns)
+    public string ProposeForeignKey(string pkSchema, string pkTable, List<string> pkColumns, string fkSchema, string fkTable, List<string> fkColumns)
     {
         if (!string.IsNullOrEmpty(pkSchema)) pkSchema += ".";
         if (!string.IsNullOrEmpty(fkSchema)) fkSchema += ".";
         
-        var pkColumnsText = string.Join(",", pkColumns);
-        var fkColumnsText = string.Join(",", fkColumns);
+       var text = @$"ALTER TABLE {fkSchema}{fkTable} ADD CONSTRAINT FK_{fkTable}{pkTable}
+                     FOREIGN KEY ({string.Join(",", pkColumns)}) REFERENCES {pkSchema}{pkTable}({string.Join(",", fkColumns)});";
 
-        var text = @$"ALTER TABLE {fkSchema}{fkTable} ADD CONSTRAINT FK_{fkTable}{pkTable}
-                     FOREIGN KEY ({fkColumnsText.SkipLast(2)}) REFERENCES {pkSchema}{pkTable}({pkColumnsText.SkipLast(2)});";
-        
-        Console.WriteLine(text);
+        return text;
     }
 }
